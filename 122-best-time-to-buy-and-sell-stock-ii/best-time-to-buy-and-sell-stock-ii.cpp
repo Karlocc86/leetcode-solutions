d9331335b2488550fdb03e8f8f52484a5a945c6c
Karlo Sebastián Cadena Cota <160698385+Karlocc86@users.noreply.github.com>
2026-05-01 18:02:10 -0600

    Time: 0 ms (100.00%) | Memory: 20 MB (80.69%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,3 +1,9 @@
+// Patrón: Greedy
+// Idea: sumar cada subida diaria es equivalente a encontrar la transacción óptima, mi ejemplo
+// donde me trabe [1,2,3,4,5], es lo mismo comprar vender diario a comprar 1 y vender en 5.
+// Si prices[i] > prices[i-1], esa diferencia forma parte de la ganancia máxima.
+// Complejidad: O(n) tiempo, O(1) espacio
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
