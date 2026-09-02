fab7798129baa22f6f3b6bf3e385df2d8ce55055
Karlo Cadena Cota <160698385+Karlocc86@users.noreply.github.com>
2026-09-02 14:28:26 -0600

    Time: 0 ms (100.00%) | Memory: 12.3 MB (71.47%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        vector<int> minFreq(26, 10000); 
+        
+        for (const string& word : words) {
+            vector<int> charCount(26, 0); 
+            
+            for (char c : word) {
+                charCount[c - 'a']++;
+            }
+            
+            for (int i = 0; i < 26; ++i) {
+                minFreq[i] = min(minFreq[i], charCount[i]);
+            }
+        }
+        
+        vector<string> result;
+        for (int i = 0; i < 26; ++i) {
+            while (minFreq[i] > 0) {
+                result.push_back(string(1, i + 'a'));
+                minFreq[i]--;
+            }
+        }
+        
+        return result;
+    }
+};
